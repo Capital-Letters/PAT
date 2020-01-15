fffef2c2cbdc0db2a91ad919ecd827d0527b6077
fu <1204650012@qq.com>
2020-01-15 15:53:55 +0800

    add

---
+++ 1041.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct info
+{
+    int id;
+    
+};
+
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        
+    }
+    
+}
